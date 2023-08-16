68886e54546529ec842edf56e54ad7544a3f46f1
unknown <abdallahshantti@gmail.com>
2023-08-16 17:33:42 +0300

    product page done

--- src/index.js
+++ src/index.js
@@ -12,4 +12,22 @@ document.querySelectorAll('.add-to-cart-btn').forEach(item => {
     })
 })
 
-console.log("helllo")
+document.querySelectorAll('.size-option input[type="radio"]').forEach(item =>{
+    item.addEventListener('change',() => {
+        document.querySelectorAll('.size-option').forEach(i =>{
+            i.classList.remove('active')
+        })
+        item.parentNode.parentNode.classList.add('active')
+    })
+})
+document.querySelectorAll('.color-option input[type="radio"]').forEach(item =>{
+    item.addEventListener('change',() => {
+        document.querySelectorAll('.color-option').forEach(i =>{
+            i.classList.remove('active')
+        })
+        item.parentNode.parentNode.classList.add('active')
+    })
+})
+
+document.getElementById("copyright").innerHTML = "جميع الحقوق محفوظة سنة " + new Date().getFullYear();
+
